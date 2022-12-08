c1d4959b950f5015bb6d12a5b348b8892ba37eba
Malibu <malibu090@mail.ru>
2022-12-09 02:03:43 +0300

    Сумма цифр заданного числа

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+int EnterNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindSum(int num)
+{
+    int sum = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int number = EnterNumber("Введите число ");
+int result = FindSum(number);
+Console.WriteLine("Сумма цифр в числе равна " + result);
